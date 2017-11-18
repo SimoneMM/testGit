c62499b09aff1a9ebdbb3d7f237c9132cc896004
小美 <1145060942@qq.com>
2017-11-18 14:49:23 +0800

    change drag.js from master

--- drag.js
+++ drag.js
@@ -6,10 +6,16 @@ function drag(id){
   var id = document.getElementById(id);
   var disX = 0;
   var disY = 0;
-  obj.onmousedown=function(ev){
-      console.log(id);
+
+  obj.onmousedown = function(ev){
+
       disX = ev.pageX - obj.offsetLeft;
       disY = ev.pageY - obj.offsetTop;
+
+      document.onmousedown = function (ev) {
+          obj.style.left = ev.pageX -disX + 'px';
+          obj.style.top = ev.pageY - disY + 'px';
+      }
   }
 
 }
