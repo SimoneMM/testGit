f912444fc7520daa187a59b1302a9e6f36b9188d
小美 <1145060942@qq.com>
2017-11-18 14:21:09 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -4,9 +4,12 @@ console.log('Hello Git');
 
 function drag(id){
   var id = document.getElementById(id);
-
-  obj.onmousedown=function(){
+  var disX = 0;
+  var disY = 0;
+  obj.onmousedown=function(ev){
       console.log(id);
+      disX = ev.pageX - obj.offsetLeft;
+      disY = ev.pageY - obj.offsetTop;
   }
 
 }
